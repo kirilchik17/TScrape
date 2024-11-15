32665ad3c543d2fe15c2ac654da37c0480d66271
Kirill Chigir <kirillchigir26@gmail.com>
2024-11-15 19:13:59 +0200

    Add project files.

---
+++ TScraper/src/main.cpp
@@ -0,0 +1,11 @@
+#include <td/telegram/Client.h>
+using namespace td;
+using namespace std;
+
+int main() {
+	auto client = make_unique<Client>();
+	auto req = make_shared<Client::Request>();
+	td_api::authC
+	auto func = make_shared<td_api::Function>();
+	client->send()
+}
